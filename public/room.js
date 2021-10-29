aecfd04110fc0b95003d1a8769294901d3bfb64b
Paolo <Paolocargnin@boom.ink>
2021-10-29 19:55:24 +0200

    prova

--- public/room.js
+++ public/room.js
@@ -11,6 +11,17 @@ const app = {
   },
   mounted() {
 
+    if ('serviceWorker' in navigator) {
+      window.addEventListener('load', function() {
+        navigator.serviceWorker.register('/sw.js').then(function(registration) {
+          // Registration was successful
+          console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }, function(err) {
+          // registration failed :(
+          console.log('ServiceWorker registration failed: ', err);
+        });
+      });
+    }
     var socket = io();
     socket.on('users_update', (users) => {
       console.log(users, this.room)
